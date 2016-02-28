74c80ea97c4f0ef67ad7fb1688c0bbee4b336f4f
rhiaro <amy@rhiaro.co.uk>
2016-02-28 15:22:25 +0000

    Get <main> and insert into template

--- scripts/do.template.js
+++ scripts/do.template.js
@@ -26,6 +26,29 @@ var TPL = {
     document.getElementById('template-last-built').textContent = time;
   },
   
+  getContent: function(url){
+      var headers = {};
+      headers['Accept'] = 'text/html; charset=utf-8';
+      return new Promise(function(resolve, reject) {
+          var http = new XMLHttpRequest();
+          http.open('GET', url);
+          Object.keys(headers).forEach(function(key) {
+              http.setRequestHeader(key, headers[key]);
+          });
+          http.withCredentials = true;
+          http.onreadystatechange = function() {
+              if (this.readyState == this.DONE) {
+                  return resolve({xhr: this});
+              }
+              // this always gets returned??? (offline tho) fuck it was because of an error in the then jesus christ
+              //return reject({status: this.status, xhr: this});
+          };
+          http.send();
+      });
+  },
+  
+  
+  
   buildContent: function(){
     var template = document;
     var pages = document.querySelectorAll('main ul a');
@@ -33,14 +56,24 @@ var TPL = {
       for(var i = 0; i < pages.length; i++){
         // TODO:
         // * GET page DOM
-        console.log(pages[i].href);
-        // * get whats in <main>
-        var pageMain = '<article><h1>Test ' + i+1 + '</h1><div><p>asdf asdf asdf!!</p></div></article>';
-        // * replace this template's <main> with content page <main>
-        template.querySelector('main').innerHTML = pageMain; // actually do this on a clone probably
-        document.getElementById('template-building').insertAdjacentHTML('beforeEnd', '<li>'+pages[i]+'</li>');
-        // * PUT that back to content page URL
-        // ** Update progress template-building list to indicate success or failure of PUT
+        var url = pages[i].href;
+        TPL.getContent(url).then(
+            function(result) {
+                console.log(result);
+                contents = document.createElement('html'); // TODO: fix
+                contents.innerHTML = result.xhr.response;
+                // * get whats in <main>
+                var pageMain = contents.querySelector('main');
+                // * replace this template's <main> with content page <main>
+                template.querySelector('main').innerHTML = pageMain.innerHTML; // actually do this on a clone probably
+                document.getElementById('template-building').insertAdjacentHTML('beforeEnd', '<li>'+url+'</li>'); // HERENOW url getting lost
+                // * PUT that back to content page URL
+                // ** Update progress template-building list to indicate success or failure of PUT
+            },
+            function(reason) {
+                console.log(reason);
+            }
+        );
       }
     }else{
       console.log('No pages to build');
